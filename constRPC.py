16e229d566eb1261e7393045c6fcd8418617bf6b
Estêvão ZorinOs <estevaobraga50@gmail.com>
2026-05-03 14:37:23 -0300

    para AWS

--- constRPC.py
+++ constRPC.py
@@ -4,9 +4,15 @@ APPEND   = '3'
 GETVALUE = '4'
 CREATE   = '5'
 STOP     = '6'
-HOSTS    = ''
-PORTS   = 50004
-PORTC1  = 50053
-PORTC2  = 50054
-HOSTC1  = ''
-HOSTC2  = ''
+
+# IP da máquina do servidor
+HOSTS  = '44.201.88.90'
+
+# Portas
+PORTS  = 50004
+PORTC1 = 50053
+PORTC2 = 50054
+
+# IPs dos clientes
+HOSTC1 = '18.210.55.23'
+HOSTC2  = '3.95.120.10'
--- run.py
+++ run.py
@@ -1,37 +1,80 @@
-import pickle, multiprocessing #-
-from socket   import *         #-
-from time     import sleep     #-
-from random   import *         #-
-from constRPC import *         #-
-#-
-from client   import *         #-
-from server   import *         #-
-from dbclient import *         #-
-#-
+import pickle
+import sys
+from time import sleep
+
+from constRPC import *
+from client import *
+from server import *
+from dbclient import *
+
+
 def client1():
-  c1   = Client(PORTC1)                # create client
-  dbC1 = DBClient(HOSTS,PORTS)         # create reference
-  dbC1.create()                        # create new list
-  dbC1.appendData('Client 1')          # append some data
-  c2 = multiprocessing.Process(target=client2).start() # create 2nd client #-
-  sleep(2)                             # make sure the other client is running #-
-  c1.sendTo(HOSTC2,PORTC2,dbC1)        # send to other client
+    print("[Client1] Iniciando...")
+
+    c1 = Client(PORTC1)
+    db = DBClient(HOSTS, PORTS)
+
+    print("[Client1] Criando lista remota...")
+    db.create()
+
+    print("[Client1] Inserindo dado...")
+    db.appendData('Client 1')
+
+    print("[Client1] Enviando referência para Client2...")
+    sleep(2)  # tempo para garantir que client2 esteja rodando
+
+    c1.sendTo(HOSTC2, PORTC2, db)
+
+    print("[Client1] Finalizado.")
+
 
 def client2():
-  c2   = Client(PORTC2)                # create a new client
-  data = c2.recvAny()                  # block until data is sent
-  dbC2 = pickle.loads(data)            # receive reference
-  dbC2.appendData('Client 2')          # append data to same list
-  print(dbC2.getValue()) #-
-  c2.sendTo(HOSTS,PORTS,[STOP]) #-
-  
-def server(): #-
-  server = Server(PORTS) #-
-  c1 = multiprocessing.Process(target=client1).start() # create 1st client #-
-  server.run() #-
-#-
-if __name__ == "__main__": #-
-#-
-  s = multiprocessing.Process(target=server) #-
-  s.start() #- 
-  s.join() #-
+    print("[Client2] Aguardando dados...")
+
+    c2 = Client(PORTC2)
+
+    data = c2.recvAny()
+    db = pickle.loads(data)
+
+    print("[Client2] Referência recebida!")
+
+    db.appendData('Client 2')
+
+    result = db.getValue()
+    print("[Client2] Lista final:", result)
+
+    # encerra servidor
+    c2.sendTo(HOSTS, PORTS, [STOP])
+
+    print("[Client2] Finalizado.")
+
+
+def server_process():
+    print("[Server] Iniciando...")
+    s = Server(PORTS)
+    s.run()
+    print("[Server] Encerrado.")
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        print("Uso:")
+        print("  python run.py server")
+        print("  python run.py client1")
+        print("  python run.py client2")
+        exit(1)
+
+    role = sys.argv[1]
+
+    if role == "server":
+        server_process()
+
+    elif role == "client1":
+        client1()
+
+    elif role == "client2":
+        client2()
+
+    else:
+        print("Role inválido:", role)
